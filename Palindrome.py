aeaa4d47d4d6abfacd04602ccd32819fb9bb732b
padmadivyapothula <119720502+padmadivyapothula@users.noreply.github.com>
2023-09-14 15:05:17 +0530

    n=int(input()) q=n s=0 while n!=0:     r=n%10     s=s*10+r     n=n//10 if s==q:     print("True") else:     print("False")

--- Palindrome.py
+++ Palindrome.py
@@ -1,11 +1,11 @@
 n=int(input())
-s=0
 q=n
-while q!=0:
-    r=q%10
+s=0
+while n!=0:
+    r=n%10
     s=s*10+r
-    q=q//10
-if s==n:
-    print("Palindrome")
+    n=n//10
+if s==q:
+    print("True")
 else:
-    print('Not Palindrome')
+    print("False")
